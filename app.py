450f26a762cf6a2d27c173d171043a0ad38873c6
Nikolay <nikolay.shevchenko.work@gmail.com>
2018-11-27 18:02:41 +0200

    New version

--- app.py
+++ app.py
@@ -1,65 +1,67 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'api_db'
-app.config['MONGO_URI'] = 'mongodb://db:27017/api_db'
+app.config['MONGO_URI'] = 'mongodb://localhost/api_db'
 
 mongo = PyMongo(app)
 
 
-@app.route('/')
-def start():
-    return 'Hello babe!'
-
-
-# Return all orders
-@app.route('/order')
-def get_all_orders():
+# use URL order?service=service&operator=operator
+@app.route('/order', methods=['GET'])
+def view_order():
     order = mongo.db.order
-
-    result = []
-
-    for q in order.find():
-        result.append({'service': q['service'], 'operator': q['operator'], 'cost': q['cost'], 'ts': q['ts']})
-
-    return jsonify({'result': result})
-
-
-# Return order
-@app.route('/order/<service>')
-def get_one_user(service):
-    order = mongo.db.order
-
-    q = order.find_one({'ts': service})
-
+    service = request.args['service']
+    operator = request.args['operator']
+    q = order.find_one({'service': service, 'operator': operator})
     if q:
-        result = {'service': q['service'], 'operator': q['operator'], 'cost': q['cost'], 'ts': q['ts']}
+        result = {'service': q['service'], 'operator': q['operator'], 'price': price}
     else:
         result = 'No results found'
 
     return jsonify({'result': result})
 
 
-# Add order
-@app.route('/order/add/<service>/<operator>/<cost>/<ts>')
-def add_order(service, operator, cost, ts):
-    order = mongo.db.order
+# use URL order?service=service&operator=Nikolay&price=200
+@app.route('/order', methods=['POST'])
+def add_order():
+    service = request.args['service']
+    operator = request.args['operator']
+    price = request.args['price']
 
-    order.insert({'service': service, 'operator': operator, 'cost': cost, 'ts': ts})
+    order = mongo.db.order
+    order.insert({'service': service, 'operator': operator, 'price': price})
 
     return 'Order added!'
 
 
-# Delete order
-@app.route('/order/delete/<service>')
-def delete_user(service):
+# use URL order?service=service&operator=Nikolay&price=200
+@app.route('/order', methods=['DELETE'])
+def delete_order():
+    order = mongo.db.order
+    service = request.args['service']
+    operator = request.args['operator']
+    price = request.args['price']
+    query = order.delete_one({'service': service, 'operator': operator, 'price': price})
+
+    if query:
+        return 'Order deleted!!'
+    else:
+        return 'Order not found!!'
+
+
+@app.route('/order/all', methods=['GET'])
+def all_orders():
     order = mongo.db.order
 
-    order.delete_one({'service': service})
+    result = []
 
-    return 'Order deleted!'
+    for q in order.find():
+        result.append({'service': q['service'], 'operator': q['operator'], 'price': q['price']})
+
+    return jsonify({'result': result})
 
 
 if __name__ == '__main__':
